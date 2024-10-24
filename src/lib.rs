cdce1015e5ac54ad28b6779eb94f11d4e897e72f
Code Monad <code@lab-11.org>
2024-10-25 03:40:00 +0800

    updating from index to indexes: utitilies

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ extern crate alloc;
 pub use crate::generated::ckbfs::{Bytes, CKBFSData};
 use alloc::{string::String, vec::Vec};
 
-use generated::ckbfs::{BackLink, BackLinkVec, Byte32, Uint32, Uint32Opt};
+use generated::ckbfs::{BackLink, BackLinkVec, Byte32, Indexes, Uint32};
 use molecule::prelude::{Builder, Entity};
 
 pub mod generated;
@@ -20,18 +20,26 @@ impl Into<Bytes> for &[u8] {
 
 #[derive(Debug, Clone)]
 pub struct BackLinkNative {
-    pub index: u32,
+    pub indexes: Vec<u32>,
     pub checksum: u32,
     pub tx_hash: [u8; 32],
 }
 
 impl Into<BackLinkNative> for BackLink {
     fn into(self) -> BackLinkNative {
-        let index = u32::from_le_bytes(self.index().as_slice().try_into().unwrap());
+        let indexes = self
+            .indexes()
+            .into_iter()
+            .map(|x| {
+                u32::from_le_bytes(x.as_slice().try_into().unwrap())
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<u32>>();
         let checksum = u32::from_le_bytes(self.checksum().as_slice().try_into().unwrap());
         let tx_hash = self.tx_hash().as_slice().try_into().unwrap();
         BackLinkNative {
-            index,
+            indexes,
             checksum,
             tx_hash,
         }
@@ -41,9 +49,15 @@ impl Into<BackLinkNative> for BackLink {
 impl Into<BackLink> for BackLinkNative {
     fn into(self) -> BackLink {
         BackLink::new_builder()
-            .index(Uint32::new_unchecked(molecule::bytes::Bytes::from(
-                self.index.to_le_bytes().to_vec(),
-            )))
+            .indexes(
+                Indexes::new_builder()
+                    .extend(self.indexes.iter().map(|x| {
+                        Uint32::new_unchecked(molecule::bytes::Bytes::from(
+                            x.to_le_bytes().to_vec(),
+                        ))
+                    }))
+                    .build(),
+            )
             .checksum(Uint32::new_unchecked(molecule::bytes::Bytes::from(
                 self.checksum.to_le_bytes().to_vec(),
             )))
@@ -56,7 +70,7 @@ impl Into<BackLink> for BackLinkNative {
 
 #[derive(Debug, Clone)]
 pub struct CKBFSDataNative {
-    pub index: Option<u32>,
+    pub indexes: Vec<u32>,
     pub checksum: u32,
     pub content_type: String,
     pub filename: String,
@@ -67,16 +81,18 @@ impl From<CKBFSDataNative> for CKBFSData {
     fn from(data: CKBFSDataNative) -> Self {
         let content_type = data.content_type.as_bytes().into();
         let filename = data.filename.as_bytes().into();
-        let index = data
-            .index
-            .map(|x| Uint32::new_unchecked(molecule::bytes::Bytes::from(x.to_le_bytes().to_vec())));
+        let indexes = data
+            .indexes
+            .iter()
+            .map(|x| Uint32::new_unchecked(molecule::bytes::Bytes::from(x.to_le_bytes().to_vec())))
+            .collect::<_>();
         let backlinks = data
             .backlinks
             .into_iter()
             .map(|backlink| backlink.into())
             .collect::<Vec<BackLink>>();
         CKBFSData::new_builder()
-            .index(Uint32Opt::new_builder().set(index).build())
+            .indexes(Indexes::new_builder().set(indexes).build())
             .checksum(Uint32::new_unchecked(molecule::bytes::Bytes::from(
                 data.checksum.to_le_bytes().to_vec(),
             )))
@@ -93,17 +109,11 @@ impl Into<CKBFSDataNative> for CKBFSData {
             .expect("Failed to extract content-type");
         let filename = String::from_utf8(self.filename().as_slice().to_vec())
             .expect("Failed to extract filname");
-        let index = match self.index().is_some() {
-            true => Some(u32::from_le_bytes(
-                self.index()
-                    .to_opt()
-                    .unwrap()
-                    .as_slice()
-                    .try_into()
-                    .unwrap(),
-            )),
-            false => None,
-        };
+        let indexes = self
+            .indexes()
+            .into_iter()
+            .map(|x| u32::from_le_bytes(x.as_slice().try_into().unwrap()))
+            .collect::<Vec<u32>>();
         let checksum = u32::from_le_bytes(self.checksum().as_slice().try_into().unwrap());
         let backlinks = self
             .backlinks()
@@ -111,7 +121,7 @@ impl Into<CKBFSDataNative> for CKBFSData {
             .map(|raw_backlink| raw_backlink.into())
             .collect::<Vec<BackLinkNative>>();
         CKBFSDataNative {
-            index,
+            indexes,
             checksum,
             content_type,
             filename,
--- tests/tests.rs
+++ tests/tests.rs
@@ -3,13 +3,13 @@ use molecule::prelude::Entity;
 #[test]
 fn test_ckbfs_hex() {
     let data = CKBFSDataNative {
-        index: Some(1),
+        indexes: vec![1],
         checksum: 3839560431,
         content_type: String::from("plain/text"),
         filename: String::from("hello_ckbfs.txt"),
         backlinks: vec![
             BackLinkNative {
-                index: 0x1,
+                indexes: vec![0x1],
                 checksum: 300548862,
                 tx_hash: hex::decode(
                     "acea79e1cbbc2cf39bf4071fd0c73f5872255a9ec8532c270d47c1c26be88985",
@@ -19,7 +19,7 @@ fn test_ckbfs_hex() {
                 .unwrap(),
             },
             BackLinkNative {
-                index: 0x1,
+                indexes: vec![0x1],
                 checksum: 1014105452,
                 tx_hash: hex::decode(
                     "2cf42e07c377f5cc2784ffb1c4e75ce51f7f291d91ff34d58c8f674de3b92060",
